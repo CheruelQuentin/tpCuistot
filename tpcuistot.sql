fcb72ba7eefdecd5ed7d709b343e7c5ad8a5cd8d
CheruelQuentin <65130218+CheruelQuentin@users.noreply.github.com>
2022-04-21 16:33:30 +0200

    update bdd + dump data

--- tpcuistot.sql
+++ tpcuistot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 21 avr. 2022 à 10:00
+-- Généré le :  jeu. 21 avr. 2022 à 14:32
 -- Version du serveur :  8.0.18
 -- Version de PHP :  7.0.33
 
@@ -25,6 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `contenir`
+--
+
+DROP TABLE IF EXISTS `contenir`;
+CREATE TABLE IF NOT EXISTS `contenir` (
+  `idRecette` int(11) NOT NULL,
+  `idIngredient` int(11) NOT NULL,
+  KEY `CONTENIR_INGREDIENT` (`idIngredient`),
+  KEY `CONTENIR_RECETTE` (`idRecette`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `contenir`
+--
+
+INSERT INTO `contenir` (`idRecette`, `idIngredient`) VALUES
+(1, 1),
+(2, 3),
+(2, 2),
+(2, 4);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `ingredients`
 --
 
@@ -32,20 +56,18 @@ DROP TABLE IF EXISTS `ingredients`;
 CREATE TABLE IF NOT EXISTS `ingredients` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nomIngredients` varchar(50) NOT NULL,
-  `idRecette` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idRecette` (`idRecette`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `ingredients`
 --
 
-INSERT INTO `ingredients` (`id`, `nomIngredients`, `idRecette`) VALUES
-(1, 'pomme de terre', 1),
-(2, 'pomme', 2),
-(3, 'calvados', 2),
-(4, 'glace', 2);
+INSERT INTO `ingredients` (`id`, `nomIngredients`) VALUES
+(1, 'pomme de terre'),
+(2, 'pomme'),
+(3, 'calvados'),
+(4, 'glace');
 
 -- --------------------------------------------------------
 
@@ -59,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `recettes` (
   `nomRecette` varchar(50) NOT NULL,
   `descriptionRecette` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `recettes`
@@ -67,17 +89,19 @@ CREATE TABLE IF NOT EXISTS `recettes` (
 
 INSERT INTO `recettes` (`id`, `nomRecette`, `descriptionRecette`) VALUES
 (1, 'Purée', 'Contient des patates'),
-(2, 'Trou Normand', 'Sorbet à la pomme avec du calva');
+(2, 'Trou Normand', 'Sorbet à la pomme avec du calva'),
+(3, 'welsh', 'Une recette du nord');
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `ingredients`
+-- Contraintes pour la table `contenir`
 --
-ALTER TABLE `ingredients`
-  ADD CONSTRAINT `ingredients_ibfk_1` FOREIGN KEY (`idRecette`) REFERENCES `recettes` (`id`);
+ALTER TABLE `contenir`
+  ADD CONSTRAINT `CONTENIR_INGREDIENT` FOREIGN KEY (`idIngredient`) REFERENCES `ingredients` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `CONTENIR_RECETTE` FOREIGN KEY (`idRecette`) REFERENCES `recettes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
