4925e73b3ef76a1f623f260572ca48e9d5720449
CheruelQuentin <65130218+CheruelQuentin@users.noreply.github.com>
2022-04-21 14:04:50 +0200

    add function to addRecette and deleteRecette

--- bd.inc.php
+++ bd.inc.php
@@ -34,6 +34,38 @@ function recettes() {
         return $resultat;
 }
 
+function addRecette($nomRecette,$descriptionRecette) {
+    $resultat = -1;
+    try {
+        $cnx = connexionPDO();
+
+        $req = $cnx->prepare("insert into recettes (nomRecette,descriptionRecette) values(:nomRecette,:descriptionRecette)");
+
+        $req->bindValue(':nomRecette', $nomRecette, PDO::PARAM_STR);
+        $req->bindValue(':descriptionRecette', $descriptionRecette, PDO::PARAM_STR);
+
+        $resultat = $req->execute();
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function deleteRecette($nomRecette) {
+    $resultat = -1;
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("delete from recettes where nomRecette=:nomRecette");
+        $req->bindValue(':nomRecette', $nomRecette, PDO::PARAM_INT);
+        $resultat = $req->execute();
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
 ini_set('soap.wsdl_cache_enabled', 0);
 $serversoap=new SoapServer("http://localhost/SOAP/TPCUISTOT/server.wsdl");
 $serversoap->addFunction("connexionPDO");
