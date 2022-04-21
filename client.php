455a4e972a7a65422b352d5a4b9cecc89f38c89d
CheruelQuentin <65130218+CheruelQuentin@users.noreply.github.com>
2022-04-21 15:22:50 +0200

    issue param addRecette

--- client.php
+++ client.php
@@ -3,6 +3,8 @@ ini_set('soap.wsdl_cache_enabled', 0);
 $service=new SoapClient("http://localhost/SOAP/TPCUISTOT/server.wsdl");
 $taballservices=$service->connexionPDO();
 $taballrecettes=$service->recettes();
+//$tabdeleterecettes=$service->deleteRecettes(1);
+$tabaddrecettes=$service->addRecette('Welsh','une recette du nord');
 
 print_r($taballrecettes);
 ?>
